2e320461f8bb330f75147bcea9ac9a399e4a321e
martinriosc <martin.rios@catena.cl>
2025-05-14 11:53:04 -0400

    push

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/v1',
+  // apiUrl: 'http://localhost:3000/api/v1',
+  apiUrl: 'https://jellyfish-app-drx97.ondigitalocean.app/api/v1',
   mapboxToken: 'pk.eyJ1IjoibWFydGlucmNydXoiLCJhIjoiY21hbWo3NGpwMGtxeTJrb2phaGt0NG1sYSJ9.21sGAtxfegVpytBs2w700g', // Reemplazar con token real
   defaultMapCenter: [-70.6483, -33.4372], // Santiago, Chile
   defaultMapZoom: 12,
