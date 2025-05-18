99a414f95e8a87a3d7917e1cdecf74c8a0b1a948
martinriosc <martin.rios@catena.cl>
2025-05-17 21:55:36 -0400

    push to main

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   // apiUrl: 'http://localhost:3000/api/v1',
   apiUrl: 'https://jellyfish-app-drx97.ondigitalocean.app/api/v1',
-  mapboxToken: 'sk.eyJ1IjoiYXllY2giLCJhIjoiY21hdDAyZzR3MG5tOTJrb2xyaHgzM2F5aCJ9.MuFQds7k4mCtIUye9Bp9Uw', // Reemplazar con token real
+  mapboxToken: 'pk.eyJ1IjoiYXllY2giLCJhIjoiY21hcjB1cDZiMDBqdDJtczJkbHp0YTZzYiJ9.t5tR9r9CDIaYizsouDccLw', // Reemplazar con token real
   defaultMapCenter: [-15.5, 28.0], // Islas Canarias, España
   defaultMapZoom: 8,
 };
