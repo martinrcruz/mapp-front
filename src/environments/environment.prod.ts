21c474c7d121a9675e2abec9d0bd1c0566d857ad
martinriosc <martin.rios@catena.cl>
2025-05-17 21:51:56 -0400

    push to main

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://jellyfish-app-drx97.ondigitalocean.app/api/v1', // En producción, el API estará en el mismo dominio
-  mapboxToken: 'pk.eyJ1IjoiZXhhbXBsZSIsImEiOiJjbGV4YW1wbGUifQ.example', // Reemplazar con token real
+  mapboxToken: 'sk.eyJ1IjoiYXllY2giLCJhIjoiY21hdDAyZzR3MG5tOTJrb2xyaHgzM2F5aCJ9.MuFQds7k4mCtIUye9Bp9Uw', // Reemplazar con token real
   defaultMapCenter: [-15.5, 28.0], // Islas Canarias, España
   defaultMapZoom: 8,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/v1',
-  // apiUrl: 'https://jellyfish-app-drx97.ondigitalocean.app/api/v1',
-  mapboxToken: 'pk.eyJ1IjoibWFydGlucmNydXoiLCJhIjoiY21hbWo3NGpwMGtxeTJrb2phaGt0NG1sYSJ9.21sGAtxfegVpytBs2w700g', // Reemplazar con token real
+  // apiUrl: 'http://localhost:3000/api/v1',
+  apiUrl: 'https://jellyfish-app-drx97.ondigitalocean.app/api/v1',
+  mapboxToken: 'sk.eyJ1IjoiYXllY2giLCJhIjoiY21hdDAyZzR3MG5tOTJrb2xyaHgzM2F5aCJ9.MuFQds7k4mCtIUye9Bp9Uw', // Reemplazar con token real
   defaultMapCenter: [-15.5, 28.0], // Islas Canarias, España
   defaultMapZoom: 8,
 };
